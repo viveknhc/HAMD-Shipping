fab1b15073781580b3a4f28c68728f7f3fa317a7
viveknhc <viveknhc@gmail.com>
2022-08-05 14:09:53 +0530

    dynamic compleated

--- static/js/main.js
+++ static/js/main.js
@@ -304,3 +304,23 @@ jQuery(window).on('scroll', function () {
 	})(jQuery);
 });
 
+
+
+// client section
+
+$('.brand-carousel').owlCarousel({
+	loop:true,
+	margin:10,
+	autoplay:true,
+	responsive:{
+	  0:{
+		items:1
+	  },
+	  600:{
+		items:3
+	  },
+	  1000:{
+		items:5
+	  }
+	}
+  })
--- web/context_processors.py
+++ web/context_processors.py
@@ -1,9 +1,15 @@
-from .models import Clients
+from .models import About, Address, Clients, Service
 
 
 def main_context(request):
     clients = Clients.objects.all()
+    address = Address.objects.filter().order_by('-id')
+    service = Service.objects.all()
+    about = About.objects.filter().order_by('-id')
     return {
         "domain": request.META["HTTP_HOST"],
         "clients":clients,
+        "address":address,
+        "service":service,
+        "about":about,
     }
